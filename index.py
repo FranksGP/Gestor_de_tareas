21c69108e7918319c2b6c516bb7b16c1419ea883
TockDoog <jorgeandreyhernandez26@gmail.com>
2025-03-27 20:11:53 -0500

    DICCIONARIO DE LOS TAKS DE EL PROYECTO se genero las las clave y los valores para los archivos json unificandolo con el print Tarea agrega exitosamente

--- index.py
+++ index.py
@@ -6,6 +6,16 @@ TASKS_FILE = "Tareas.json"
 def cargarTask():
     if os.path.exists(TASKS_FILE):
         with open(TASKS_FILE, "r") as file:
-            return json.load(file)
+            return json.load(file)  
+        return []
 
+def cargarTask(tasks):
+    with open(TASKS_FILE, "w") as file:
+        json.dump(tasks, file, indent=4)
+
+def agregarTask(titulo, descripcion):
+    tasks = cargarTask()
+    tasks.append({"titulo": titulo, "descripcion": descripcion, "completed": False})
+    cargarTask(tasks)
+    print("Tarea agregada exitosamente.")
 

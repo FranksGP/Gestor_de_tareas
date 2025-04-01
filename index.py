559ee96155ac84dd71bda82daab74a6d5df2f08a
Dissel <camiloleal2@outlook.es>
2025-03-31 19:20:21 -0500

    Agregue la funcion para que se cargue el archivo .json

--- index.py
+++ index.py
@@ -7,5 +7,13 @@ def cargarTask():
     if os.path.exists(TASKS_FILE):
         with open(TASKS_FILE, "r") as file:
             return json.load(file)
+        return []
+
+def cargarTask(tasks):
+    with open(TASKS_FILE, "w") as file:
+        json.dump(tasks, file, indent=4)
+
+
+
 
 
